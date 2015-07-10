bade1f6a432cb48fa7e4e5f5b43b102d0166037e
s-hashimoto <s-hashimoto@oz-vision.co.jp>
2015-07-10 21:14:50 +0900

    valid_moneysを変更した
    不加算名詞のため

--- app/MoneyCheck.php
+++ app/MoneyCheck.php
@@ -4,17 +4,17 @@ class MoneyCheck
 	/**
 	 * 許可されている金額
 	 */
-	private static $_valid_moneys = array(10, 50, 100, 500, 1000);
+	private static $_valid_money = array(10, 50, 100, 500, 1000);
 
 	/**
 	 * 許可されている金額かを確認する
 	 *
-	 * @param integer $amount 投入金額
+	 * @param integer $amount 金額
 	 *
 	 * @return boolean
 	 */
 	public function is_valid_money($amount)
 	{
-		return in_array($amount, self::$_valid_moneys, true);
+		return in_array($amount, self::$_valid_money, true);
 	}
 }
