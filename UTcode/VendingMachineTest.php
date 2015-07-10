867ca5c6498ce499a082aa3a5b75ff107f64ee9b
y-terashima <y-terashima@oz-vision.co.jp>
2015-07-10 21:55:35 +0900

    [WIP]arrayパターンのテスト

--- UTcode/VendingMachineTest.php
+++ UTcode/VendingMachineTest.php
@@ -28,58 +28,84 @@ class VendingMachineTest extends PHPUnit_Framework_TestCase
 	}
 
 	/**
-	 *
+	 * take_money() のテスト
 	 */
 	public function test_take_money_10円を入れる()
 	{
-		$this->assertequals($this->object->take_money(10), 10);
+		$this->assertequals(10, $this->object->take_money(10));
 	}
 
 	public function test_take_money_50円を入れる()
 	{
-		$this->assertequals($this->object->take_money(50), 50);
+		$this->assertequals(50, $this->object->take_money(50));
 	}
 
 	public function test_take_money_100円を入れる()
 	{
-		$this->assertequals($this->object->take_money(100), 100);
+		$this->assertequals(100, $this->object->take_money(100));
 	}
 
 	public function test_take_money_500円を入れる()
 	{
-		$this->assertequals($this->object->take_money(500), 500);
+		$this->assertequals(500, $this->object->take_money(500));
 	}
 
 	public function test_take_money_1000円を入れる()
 	{
-		$this->assertequals($this->object->take_money(1000), 1000);
+		$this->assertequals(1000, $this->object->take_money(1000));
 	}
 
 	public function test_take_money_1円を入れる()
 	{
-		$this->assertequals($this->object->take_money(1), 0);
+		$this->assertequals(0, $this->object->take_money(1));
 	}
 
 	public function test_take_money_5円を入れる()
 	{
-		$this->assertequals($this->object->take_money(5), 0);
+		$this->assertequals(0, $this->object->take_money(5));
+	}
+
+	public function test_take_money_2000円を入れる()
+	{
+		$this->assertequals(0, $this->object->take_money(2000));
+	}
+
+	public function test_take_money_5000円を入れる()
+	{
+		$this->assertequals(0, $this->object->take_money(5000));
+	}
+
+	public function test_take_money_10000円を入れる()
+	{
+		$this->assertequals(0, $this->object->take_money(10000));
 	}
 
 	public function test_take_money_10円と100円を入れる()
 	{
-		$this->assertequals($this->object->take_money(10), 10);
-		$this->assertequals($this->object->take_money(100), 110);
+		$this->assertequals(10, $this->object->take_money(10));
+		$this->assertequals(110, $this->object->take_money(100));
 	}
 
 	public function test_take_money_10円と1円を入れる()
 	{
-		$this->assertequals($this->object->take_money(10), 10);
-		$this->assertequals($this->object->take_money(1), 10);
+		$this->assertequals(10, $this->object->take_money(10));
+		$this->assertequals(10, $this->object->take_money(1));
 	}
 
 	public function test_take_money_1円と10円を入れる()
 	{
-		$this->assertequals($this->object->take_money(1), 0);
-		$this->assertequals($this->object->take_money(10), 10);
+		$this->assertequals(0, $this->object->take_money(1));
+		$this->assertequals(10, $this->object->take_money(10));
 	}
+
+	public function test_take_money_10円と100円を一緒に入れる()
+	{
+		$this->assertequals(0, $this->object->take_money(array()));
+	}
+
+	/**
+	 * pay_back()のUnitTest
+	 */
+
+
 }
