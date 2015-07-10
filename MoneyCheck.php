9e5de98e365ff96d1f96cd32276b181882e18a7c
Yuko Terashima <y-terashima@oz-vision.co.jp>
2015-07-10 16:49:56 +0900

    変数名を変えてみた
    まだ使われていないと思うので、変更はここだけかな。

--- MoneyCheck.php
+++ MoneyCheck.php
@@ -2,7 +2,7 @@
 class MoneyCheck
 {
 	//許可されている金額
-	private static $_allowed_single_amount_values = array(10, 50, 100, 500, 1000);
+	private static $_valid_money = array(10, 50, 100, 500, 1000);
 
 	/**
 	 * 許可されているお金かを確認する
