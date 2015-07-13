db11334b3e32f7ba72c2a0929d961cd9ec3a5b8f
sakumashunsuke <s-sakuma@oz-vision.co.jp>
2015-07-13 19:35:05 +0900

    プロダクションコードの修正
    テストコードも種類を増やした

--- UTcode/TrayTest.php
+++ UTcode/TrayTest.php
@@ -24,9 +24,21 @@ class TrayTest extends PHPUnit_Framework_TestCase
 	 * 今は、何を渡してもTrueが返っちゃうよ。（0以外）
 	 * 修正してね
 	 */
-	public function test_add_amountを呼ぶ()
+	public function test_add_amountを呼ぶINTを渡す()
 	{
-		$this->assertTrue($this->object->add_amount(1.1));
+		$this->assertTrue($this->object->add_amount(100));
+		$this->assertTrue($this->object->add_amount(100));
+	}
+
+	public function test_add_amountを呼ぶ小数値を渡す()
+	{
+		$this->assertFalse($this->object->add_amount(1.1));
+	}
+
+	public function test_add_amountを呼ぶ配列を渡す()
+	{
+		$this->assertFalse($this->object->add_amount(array(1,1)));
+		$this->assertFalse($this->object->add_amount(array("a"=>1)));
 	}
 
 	public function test_get_amountを呼ぶ()
@@ -35,5 +47,11 @@ class TrayTest extends PHPUnit_Framework_TestCase
 		$this->object->add_amount(100);
 		$this->assertEquals(200, $this->object->get_amount());
 	}
-	
+
+	public function test_get_amountを呼ぶ減算パターン()
+	{
+		$this->object->add_amount(100);
+		$this->object->add_amount(-100);
+		$this->assertEquals(0, $this->object->get_amount());
+	}
 }
