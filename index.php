103f400d6c3cfdbd8ae7c1f1bdd3650108b67b1d
y-kosuge <y-kosuge@oz-vision.co.jp>
2015-07-13 21:07:50 +0900

    基本操作のボタンを追加

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php
-	phpinfo();
-?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<title>きなこもち</title>
+</head>
+<body>
+	<ul>
+		<li>
+			<button>お金を入れる</button>
+		</li>
+		<li>
+			<button>払い戻す</button>
+		</li>
+		<li>
+			<button>トレイからとる</button>
+		</li>
+	</ul>
+</body>
+</html>
