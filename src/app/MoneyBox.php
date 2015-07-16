da58cc6166ad5ede06e2fbfd6b55f986c478fed2
s-hashimoto <s-hashimoto@oz-vision.co.jp>
2015-07-16 21:18:43 +0900

    MoneyBox作成
    お金管理さん作成
    トレイが、トレイ以外の仕事していたため

---
+++ src/app/MoneyBox.php
@@ -0,0 +1,14 @@
+<?php
+class MoneyBox
+{
+	//総計金額 intger
+
+	//__constractに総計金額の初期化をもたせる
+
+	//function お金をもらう intger public
+	//vendingmachineから、チェックの通った投入金額お金もらう
+
+
+	//function お金をあげる intger pubiic
+	//総計金額をすべてかえる（レバー引かれたとき）
+	//総計を０にする
