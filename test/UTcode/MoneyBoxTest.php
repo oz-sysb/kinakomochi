d299ec9e716e8c77807263f078ef6a087dfbf257
s-hashimoto <s-hashimoto@oz-vision.co.jp>
2015-07-16 22:18:38 +0900

    testケース追加
    add_totalのパターンを増やしたのと、total_listのテストケース作成

--- test/UTcode/MoneyBoxTest.php
+++ test/UTcode/MoneyBoxTest.php
@@ -44,4 +44,40 @@ class MoneyBoxTest extends PHPUnit_Framework_TestCase
 					[10000]
 				];
 	}
+
+	/**
+	 * @dataProvider total_list
+	 * @test
+	 */
+	public function test_return_all($money,$money2,$money3,$expected)
+	{
+		$this->money_box->add_total($money);
+		$this->money_box->add_total($money2);
+		$result = $this->money_box->add_total($money3);
+		$this->assertEquals($result, $expected);
+		$result = $this->money_box->return_all();
+		$this->assertEquals($result, $expected);
+	}
+
+	/**
+	 * お金リスト
+	 * @return array
+	 */
+	public function total_list()
+	{
+		return [
+					[10,50,100,160],
+					[50,5000,500,5550],
+					[10000,1,5,10006]
+				];
+	}
+
+	/**
+	 * @test
+	 */
+	public function test_return_all_zero()
+	{
+		$result = $this->money_box->return_all();
+		$this->assertEquals(0, $result);
+	}
 }
