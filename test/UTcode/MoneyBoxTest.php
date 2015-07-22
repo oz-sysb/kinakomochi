ede92e0ce3d36a007926ca68a723dd1fe7b996ef
y-terashima <y-terashima@oz-vision.co.jp>
2015-07-22 20:07:47 +0900

    タブを4スペースに修正

--- test/UTcode/MoneyBoxTest.php
+++ test/UTcode/MoneyBoxTest.php
@@ -4,58 +4,58 @@ require_once 'src/app/MoneyBox.php';
 
 class MoneyBoxTest extends PHPUnit_Framework_TestCase
 {
-	/** @var MoneyCheck */
-	private $money_box;
+    /** @var MoneyCheck */
+    private $money_box;
 
-	/**
-	 * @setup
-	 */
-	public function setUp()
-	{
-		$this->money_box = new MoneyBox();
-	}
+    /**
+     * @setup
+     */
+    public function setUp()
+    {
+        $this->money_box = new MoneyBox();
+    }
 
-	/**
-	 * @dataProvider money_list
-	 * @test
-	 */
-	public function test_add_total($money)
-	{
-		$result = $this->money_box->addTotal($money);
-		$this->assertEquals($result, $money);
-	}
+    /**
+     * @dataProvider money_list
+     * @test
+     */
+    public function test_add_total($money)
+    {
+        $result = $this->money_box->addTotal($money);
+        $this->assertEquals($result, $money);
+    }
 
-	/**
-	 * お金リスト
-	 * @return array
-	 */
-	public function money_list()
-	{
-		return [
-					[1],
-					[5],
-					[10],
-					[50],
-					[100],
-					[500],
-					[1000],
-					[2000],
-					[5000],
-					[10000]
-				];
-	}
+    /**
+     * お金リスト
+     * @return array
+     */
+    public function money_list()
+    {
+        return [
+            [1],
+            [5],
+            [10],
+            [50],
+            [100],
+            [500],
+            [1000],
+            [2000],
+            [5000],
+            [10000]
+        ];
+    }
 
-	/**
-	 * お金リスト
-	 * @return array
-	 */
-	public function total_list()
-	{
-		return [
-					[10,50,100,160],
-					[50,5000,500,5550],
-					[10000,1,5,10006]
-				];
-	}
+    /**
+     * お金リスト
+     * @return array
+     */
+    public function total_list()
+    {
+        return [
+            [10,50,100,160],
+            [50,5000,500,5550],
+            [10000,1,5,10006]
+        ];
+    }
 
 }
