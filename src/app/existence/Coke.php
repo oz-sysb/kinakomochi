bba9eb9124e79cf73897aa2f8198db7632134a4b
s-hashimoto <s-hashimoto@oz-vision.co.jp>
2015-07-23 20:54:34 +0900

    コーラオブジェクト作成
    コーラを配列で管理したくなくて、オブジェクトとして管理したいため

---
+++ src/app/existence/Coke.php
@@ -0,0 +1,27 @@
+<?php
+namespace app\existence;
+class Coke implements DrinkInterface
+{
+    private $name;
+    private $price;
+    private $count;
+
+		/**
+		 * コンストラクタ
+		 */
+    public function __construct()
+    {
+        $this->name = "コーラ";
+        $this->price = 120;
+        $this->number = 5;
+    }
+
+		/**
+		 * 商品名返す
+		 * @return string
+		 */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
