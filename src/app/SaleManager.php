7b8f0894c783ccebca30ba08d864c65d2e41d22a
y-terashima <y-terashima@oz-vision.co.jp>
2015-07-24 19:56:34 +0900

    なくていいコメントは消したくらいです。

--- src/app/SaleManager.php
+++ src/app/SaleManager.php
@@ -1,22 +1,8 @@
 <?php
 namespace VendingMachine;
 
-//require_once 'MoneyBox.php';
-//require_once 'Tray.php';
-//require_once 'MoneyCheck.php';
-
-/**
- * 購入管理クラス
- *
- * @category PHP
- * @package  VOID
- * @author   Shunsuke Sakuma <s-sakuma@oz-vision.co.jp>
- * @license  BSD Licence
- * @link     http://github.com/oz-sysb/kinakomochi
- */
 class SaleManager
 {
-
     /**
      * コンストラクタ
      */
@@ -24,8 +10,16 @@ class SaleManager
     {
     }
 
+    /**
+     * 購入できるジュースの名前を返却する
+     *
+     * @param integer $amount 投入金額の総計
+     *
+     * @return string
+     */
     public function checkBought($amount)
     {
+
         //在庫のじゅーすがある
         //$amountで返るじゅーすがあるかJuice_objから判断する
         //買えるものがなければvoidを返却する
